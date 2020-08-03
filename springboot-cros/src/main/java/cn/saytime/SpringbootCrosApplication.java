3e406c8ebb2fc6ae5496b3d712645a133ac6d93b
xinxin.niu <531955671@qq.com>
2020-08-03 16:54:03 +0800

    20200803 扫描bean

--- springboot-cros/src/main/java/cn/saytime/SpringbootCrosApplication.java
+++ springboot-cros/src/main/java/cn/saytime/SpringbootCrosApplication.java
@@ -2,8 +2,11 @@ package cn.saytime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//扫描bean来调包
+@ComponentScan(basePackages = {"com.uih.uplus.common","cn.saytime"})
 public class SpringbootCrosApplication {
 
 	public static void main(String[] args) {
